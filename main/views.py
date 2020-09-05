f611490dc95cc6be8dd86ed7e035324052d66e6d
Alexander <yspresi@yandex.ru>
2020-09-06 00:24:29 +0300

    added example of index page without content

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render # noqa
+from django.shortcuts import render
 
-# Create your views here.
+from .models import Recipe
+
+
+def index(request):
+    # главная страница
+    recipe_list = Recipe.objects.all()
+    return render(request, "indexNotAuth.html", {"recipe_list": recipe_list}, )
