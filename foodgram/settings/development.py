fe3f22a671ee424c4f133e2560066e09b428dee5
Alexander <yspresi@yandex.ru>
2020-09-05 19:07:25 +0300

    gebug-toolbar added for development

--- foodgram/settings/development.py
+++ foodgram/settings/development.py
@@ -2,10 +2,19 @@ from .base import * # noqa
 
 
 DEBUG = True
-
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
-
 STATICFILES_DIR = os.path.join(BASE_DIR, 'static') # noqa
+
+# settings for debug-toolbar
+INSTALLED_APPS += [ # noqa
+    "debug_toolbar",
+]
+MIDDLEWARE += [ # noqa
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 # Logging
 if DEBUG:
--- foodgram/urls.py
+++ foodgram/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
 
+# urls for static and media
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
@@ -29,3 +30,8 @@ if settings.DEBUG:
     urlpatterns += static(
         settings.STATIC_URL, document_root=settings.STATIC_ROOT
     )
+
+# urls for debug-toolbar
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += (path("__debug__/", include(debug_toolbar.urls)),)
