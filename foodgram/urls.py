49052568fc836212463b09329f24884bda2927a9
Alexander <yspresi@yandex.ru>
2020-09-17 22:41:09 +0300

    added logout and reset password

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -29,6 +29,26 @@ urlpatterns = [
     path('auth/', include([
         path('login/', auth_view.LoginView.as_view(
             template_name='authForm.html'), name='login'),
+        path('logout/', auth_view.LogoutView.as_view(
+            template_name='logged_out.html'), name='logout'),
+        path('password_change/', auth_view.PasswordChangeView.as_view(
+            template_name='changePassword.html'), name='password_change'),
+        path('password_change/done/',
+             auth_view.PasswordChangeDoneView.as_view(
+                template_name='password_change_done.html'
+                ), name='password_change_done'),
+        path('password_reset/',
+             auth_view.PasswordResetView.as_view(
+                template_name='resetPassword.html'
+                ), name='password_reset'),
+        path('reset/<uidb64>/<token>/',
+             auth_view.PasswordResetConfirmView.as_view(
+                template_name='password_reset_confirm.html'
+                ), name='password_reset_confirm'),
+        path('password_reset/done/',
+             auth_view.PasswordResetDoneView.as_view(
+                template_name='password_reset_done.html'
+                ), name='password_reset_done'),
     ])),
     path('api/v1/', include('api.urls')),
 ]
