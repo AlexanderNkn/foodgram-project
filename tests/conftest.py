b6ef96e68d3cb9aa7c6bc00ec56526e9d96ec274
Alexander <yspresi@yandex.ru>
2020-09-23 10:13:40 +0300

    changed pytest.ini

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import sys
 from os.path import abspath
 from os.path import dirname
 
-root_dir = dirname(dirname(dirname(abspath(__file__))))
+root_dir = dirname(dirname(abspath(__file__)))
 sys.path.append(root_dir)
 
 
