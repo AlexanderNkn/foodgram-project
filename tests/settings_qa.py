5fe6c3be7aa00184418aadcfc7d8cad9f642be6b
Alexander <yspresi@yandex.ru>
2020-09-23 08:50:05 +0300

    change pytest settings

--- tests/settings_qa.py
+++
@@ -1,8 +0,0 @@
-from foodgram.settings.development import *  # noqa
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # noqa
-    }
-}
